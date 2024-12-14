a91fc06e364db40445e8a26a4391bfc3edd75d9e
SparrowBrain <qwxmail@gmail.com>
2024-12-14 21:05:02 +0200

    Added PlaytimeCalendarAggregator

---
+++ YearInReview/Model/Aggregators/Data/CalendarDay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YearInReview.Model.Aggregators.Data
+{
+	public class CalendarDay
+	{
+		public DateTime Date { get; set; }
+
+		public int TotalPlaytime { get; set; }
+
+		public List<GameWithTime> Games { get; set; }
+	}
+}
---
+++ YearInReview/Model/Aggregators/Data/PlaytimeCalendar.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YearInReview.Model.Aggregators.Data
+{
+	public class PlaytimeCalendar
+	{
+		public IDictionary<DateTime, CalendarDay> Days { get; set; }
+	}
+}
