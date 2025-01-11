456cb3bb66366b66b608f5e077200fb42adfbd56
JonasPTFL <js.pp@gmx.de>
2025-01-11 14:36:21 +0100

    implements added game test class to mock playnite api instead of providing parameters to sut directly according to pull request #15 - adds implementation to TestableItemCollection.cs, which is needed in AddedGamesAggregatorTest.cs

--- YearInReview.UnitTests/Model/Aggregators/AddedGamesAggregatorTest.cs
+++ YearInReview.UnitTests/Model/Aggregators/AddedGamesAggregatorTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using AutoFixture.Xunit2;
+using FakeItEasy;
+using Playnite.SDK;
 using Playnite.SDK.Models;
 using TestTools.Shared;
 using Xunit;
@@ -8,166 +10,219 @@ using YearInReview.Model.Aggregators;
 
 namespace YearInReview.UnitTests.Model.Aggregators
 {
-	public class AddedGamesAggregatorTest
-	{
-		[Theory]
-		[AutoFakeItEasyData]
-		public void GetAddedGames_Empty_WhenGamesNull(AddedGamesAggregator sut)
-		{
-			// Arrange
-			List<Game> games = null;
+    public class AddedGamesAggregatorTest
+    {
+        [Theory]
+        [AutoFakeItEasyData]
+        public void GetAddedGames_Empty_WhenGamesNull(
+            [Frozen] IGameDatabaseAPI gameDatabaseApiFake,
+            [Frozen] IPlayniteAPI playniteApiFake,
+            AddedGamesAggregator sut
+        )
+        {
+            // Arrange
+            List<Game> games = null;
+            MockPlayniteApiGames(gameDatabaseApiFake, playniteApiFake, games);
 
-			// Act
-			var result = sut.GetAddedGames(games, 0);
+            // Act
+            var result = sut.GetAddedGames(0);
 
-			// Assert
-			Assert.Empty(result);
-		}
+            // Assert
+            Assert.Empty(result);
+        }
 
-		[Theory]
-		[AutoFakeItEasyData]
-		public void GetAddedGames_Empty_WhenContainsNullGame(AddedGamesAggregator sut)
-		{
-			// Arrange
-			List<Game> games = new List<Game>() { null };
+        [Theory]
+        [AutoFakeItEasyData]
+        public void GetAddedGames_Empty_WhenContainsNullGame(
+            [Frozen] IGameDatabaseAPI gameDatabaseApiFake,
+            [Frozen] IPlayniteAPI playniteApiFake,
+            AddedGamesAggregator sut
+        )
+        {
+            // Arrange
+            List<Game> games = new List<Game> { null };
+            MockPlayniteApiGames(gameDatabaseApiFake, playniteApiFake, games);
 
-			// Act
-			var result = sut.GetAddedGames(games, 0);
+            // Act
+            var result = sut.GetAddedGames(0);
 
-			// Assert
-			Assert.Empty(result);
-		}
+            // Assert
+            Assert.Empty(result);
+        }
 
-		[Theory]
-		[AutoFakeItEasyData]
-		public void GetAddedGames_Empty_WhenGamesEmpty(AddedGamesAggregator sut)
-		{
-			// Arrange
-			var games = new List<Game>();
+        [Theory]
+        [AutoFakeItEasyData]
+        public void GetAddedGames_Empty_WhenGamesEmpty(
+            [Frozen] IGameDatabaseAPI gameDatabaseApiFake,
+            [Frozen] IPlayniteAPI playniteApiFake,
+            AddedGamesAggregator sut
+        )
+        {
+            // Arrange
+            var games = new List<Game>();
+            MockPlayniteApiGames(gameDatabaseApiFake, playniteApiFake, games);
 
-			// Act
-			var result = sut.GetAddedGames(games, 0);
+            // Act
+            var result = sut.GetAddedGames(0);
 
-			// Assert
-			Assert.Empty(result);
-		}
-		
-		[Theory] 
-		[AutoFakeItEasyData] 
-		public void GetAddedGames_Empty_WhenGameAddedDateNull(AddedGamesAggregator sut)
-		{
-			// Arrange
-			var games = new List<Game>();
-			var game = new Game { Added = null };
-			games.Add(game);
+            // Assert
+            Assert.Empty(result);
+        }
 
-			// Act
-			var result = sut.GetAddedGames(games, 2025);
+        [Theory]
+        [AutoFakeItEasyData]
+        public void GetAddedGames_Empty_WhenGameAddedDateNull(
+            [Frozen] IGameDatabaseAPI gameDatabaseApiFake,
+            [Frozen] IPlayniteAPI playniteApiFake,
+            AddedGamesAggregator sut
+        )
+        {
+            // Arrange
+            var game = new Game { Added = null };
+            var games = new List<Game> { game };
+            MockPlayniteApiGames(gameDatabaseApiFake, playniteApiFake, games);
 
-			// Assert
-			Assert.Empty(result);
-		}
+            // Act
+            var result = sut.GetAddedGames(2025);
 
-		[Theory] 
-		[AutoFakeItEasyData] 
-		public void GetAddedGames_ReturnsGame_WhenGameMatching(AddedGamesAggregator sut)
-		{
-			// Arrange
-			var date = new DateTime(2025, 1, 1);
-			var games = new List<Game>();
-			var game = new Game { Added = date };
-			games.Add(game);
+            // Assert
+            Assert.Empty(result);
+        }
 
-			// Act
-			var result = sut.GetAddedGames(games, date.Year);
+        [Theory]
+        [AutoFakeItEasyData]
+        public void GetAddedGames_ReturnsGame_WhenGameMatching(
+            [Frozen] IGameDatabaseAPI gameDatabaseApiFake,
+            [Frozen] IPlayniteAPI playniteApiFake,
+            AddedGamesAggregator sut
+        )
+        {
+            // Arrange
+            var date = new DateTime(2025, 1, 1);
+            var game = new Game { Added = date };
+            var games = new List<Game> { game };
+            MockPlayniteApiGames(gameDatabaseApiFake, playniteApiFake, games);
 
-			// Assert
-			var assertedAddedGame = Assert.Single(result);
-			Assert.Equal(game, assertedAddedGame.Game);
-		}
+            // Act
+            var result = sut.GetAddedGames(date.Year);
 
-		[Theory] 
-		[AutoFakeItEasyData] 
-		public void GetAddedGames_Empty_WhenGameNotMatching(AddedGamesAggregator sut)
-		{
-			// Arrange
-			var games = new List<Game>();
-			var game = new Game { Added = new DateTime(2025, 1, 1) };
-			games.Add(game);
+            // Assert
+            var assertedAddedGame = Assert.Single(result);
+            Assert.Equal(game, assertedAddedGame.Game);
+        }
 
-			// Act
-			var result = sut.GetAddedGames(games, 2020);
+        [Theory]
+        [AutoFakeItEasyData]
+        public void GetAddedGames_Empty_WhenGameNotMatching(
+            [Frozen] IGameDatabaseAPI gameDatabaseApiFake,
+            [Frozen] IPlayniteAPI playniteApiFake,
+            AddedGamesAggregator sut
+        )
+        {
+            // Arrange
+            var games = new List<Game>();
+            var game = new Game { Added = new DateTime(2025, 1, 1) };
+            games.Add(game);
+            MockPlayniteApiGames(gameDatabaseApiFake, playniteApiFake, games);
 
-			// Assert
-			Assert.Empty(result);
-		}
+            // Act
+            var result = sut.GetAddedGames(2020);
 
-		[Theory] 
-		[AutoFakeItEasyData] 
-		public void GetAddedGames_ReturnsOneCorrect_WhenOneMatchingOneNotMatchingGame(AddedGamesAggregator sut)
-		{
-			// Arrange
-			var dateMatching = new DateTime(2025, 1, 1);
-			var dateNotMatching = new DateTime(2000, 1, 1);
-			var matchingGame = new Game { Added = dateMatching };
-			var notMatchingGame = new Game { Added = dateNotMatching };
-			
-			var games = new List<Game> { matchingGame, notMatchingGame };
+            // Assert
+            Assert.Empty(result);
+        }
 
-			// Act
-			var result = sut.GetAddedGames(games, dateMatching.Year);
+        [Theory]
+        [AutoFakeItEasyData]
+        public void GetAddedGames_ReturnsOneCorrect_WhenOneMatchingOneNotMatchingGame(
+            [Frozen] IGameDatabaseAPI gameDatabaseApiFake,
+            [Frozen] IPlayniteAPI playniteApiFake,
+            AddedGamesAggregator sut
+        )
+        {
+            // Arrange
+            var dateMatching = new DateTime(2025, 1, 1);
+            var dateNotMatching = new DateTime(2000, 1, 1);
+            var matchingGame = new Game { Added = dateMatching };
+            var notMatchingGame = new Game { Added = dateNotMatching };
 
-			// Assert
-			var assertedAddedGame = Assert.Single(result);
-			Assert.Equal(matchingGame, assertedAddedGame.Game);
-		}
-		
-		[Theory]
-		[AutoFakeItEasyData]
-		public void GetAddedGames_ReturnsCorrectGames_WhenMultipleGamesWithSameAddedDate(AddedGamesAggregator sut)
-		{
-			// Arrange
-			var date = new DateTime(2025, 1, 1);
-			var game1 = new Game { Added = date };
-			var game2 = new Game { Added = date };
-			var game3 = new Game { Added = date };
+            var games = new List<Game> { matchingGame, notMatchingGame };
+            MockPlayniteApiGames(gameDatabaseApiFake, playniteApiFake, games);
 
-			var games = new List<Game> { game1, game2, game3 };
+            // Act
+            var result = sut.GetAddedGames(dateMatching.Year);
 
-			// Act
-			var result = sut.GetAddedGames(games, date.Year);
+            // Assert
+            var assertedAddedGame = Assert.Single(result);
+            Assert.Equal(matchingGame, assertedAddedGame.Game);
+        }
 
-			// Assert
-			Assert.Equal(3, result.Count);
-			Assert.Contains(result, r => Equals(r.Game, game1) && r.AddedDate == date);
-			Assert.Contains(result, r => Equals(r.Game, game2) && r.AddedDate == date);
-			Assert.Contains(result, r => Equals(r.Game, game3) && r.AddedDate == date);
-		}
+        [Theory]
+        [AutoFakeItEasyData]
+        public void GetAddedGames_ReturnsCorrectGames_WhenMultipleGamesWithSameAddedDate(
+            [Frozen] IGameDatabaseAPI gameDatabaseApiFake,
+            [Frozen] IPlayniteAPI playniteApiFake,
+            AddedGamesAggregator sut
+        )
+        {
+            // Arrange
+            var date = new DateTime(2025, 1, 1);
+            var game1 = new Game { Added = date };
+            var game2 = new Game { Added = date };
+            var game3 = new Game { Added = date };
 
-		[Theory]
-		[AutoFakeItEasyData]
-		public void GetAddedGames_ReturnsOnlyMatching_WhenIncludesGamesAddedOnBoundaryDates(AddedGamesAggregator sut)
-		{
-			// Arrange
-			const int year = 2025;
-			var firstDayOfYear = new DateTime(year, 1, 1);
-			var lastDayOfYear = new DateTime(year, 12, 31);
-			var gameOnFirstDay = new Game { Added = firstDayOfYear };
-			var gameOnLastDay = new Game { Added = lastDayOfYear };
-			var gameOutsideYear1 = new Game { Added = new DateTime(year + 1, 1, 1) };
-			var gameOutsideYear2 = new Game { Added = new DateTime(year - 1, 12, 31) };
+            var games = new List<Game> { game1, game2, game3 };
+            MockPlayniteApiGames(gameDatabaseApiFake, playniteApiFake, games);
 
-			var games = new List<Game> { gameOnFirstDay, gameOnLastDay, gameOutsideYear1, gameOutsideYear2 };
+            // Act
+            var result = sut.GetAddedGames(date.Year);
 
-			// Act
-			var result = sut.GetAddedGames(games, year);
+            // Assert
+            Assert.Equal(3, result.Count);
+            Assert.Contains(result, r => Equals(r.Game, game1) && r.AddedDate == date);
+            Assert.Contains(result, r => Equals(r.Game, game2) && r.AddedDate == date);
+            Assert.Contains(result, r => Equals(r.Game, game3) && r.AddedDate == date);
+        }
 
-			// Assert
-			Assert.Equal(2, result.Count);
-			Assert.Contains(result, r => Equals(r.Game, gameOnFirstDay) && r.AddedDate == firstDayOfYear);
-			Assert.Contains(result, r => Equals(r.Game, gameOnLastDay) && r.AddedDate == lastDayOfYear);
-			Assert.DoesNotContain(result, r => Equals(r.Game, gameOutsideYear1));
-			Assert.DoesNotContain(result, r => Equals(r.Game, gameOutsideYear2));
-		}
-	}
+        [Theory]
+        [AutoFakeItEasyData]
+        public void GetAddedGames_ReturnsOnlyMatching_WhenIncludesGamesAddedOnBoundaryDates(
+            [Frozen] IGameDatabaseAPI gameDatabaseApiFake,
+            [Frozen] IPlayniteAPI playniteApiFake,
+            AddedGamesAggregator sut
+        )
+        {
+            // Arrange
+            const int year = 2025;
+            var firstDayOfYear = new DateTime(year, 1, 1);
+            var lastDayOfYear = new DateTime(year, 12, 31);
+            var gameOnFirstDay = new Game { Added = firstDayOfYear };
+            var gameOnLastDay = new Game { Added = lastDayOfYear };
+            var gameOutsideYear1 = new Game { Added = new DateTime(year + 1, 1, 1) };
+            var gameOutsideYear2 = new Game { Added = new DateTime(year - 1, 12, 31) };
+
+            var games = new List<Game> { gameOnFirstDay, gameOnLastDay, gameOutsideYear1, gameOutsideYear2 };
+            MockPlayniteApiGames(gameDatabaseApiFake, playniteApiFake, games);
+
+            // Act
+            var result = sut.GetAddedGames(year);
+
+            // Assert
+            Assert.Equal(2, result.Count);
+            Assert.Contains(result, r => Equals(r.Game, gameOnFirstDay) && r.AddedDate == firstDayOfYear);
+            Assert.Contains(result, r => Equals(r.Game, gameOnLastDay) && r.AddedDate == lastDayOfYear);
+            Assert.DoesNotContain(result, r => Equals(r.Game, gameOutsideYear1));
+            Assert.DoesNotContain(result, r => Equals(r.Game, gameOutsideYear2));
+        }
+
+        private static void MockPlayniteApiGames(
+            IGameDatabaseAPI gameDatabaseApiFake,
+            IPlayniteAPI playniteApiFake,
+            List<Game> games
+        )
+        {
+            A.CallTo(() => gameDatabaseApiFake.Games).Returns(new TestableItemCollection<Game>(games));
+            A.CallTo(() => playniteApiFake.Database).Returns(gameDatabaseApiFake);
+        }
+    }
 }
